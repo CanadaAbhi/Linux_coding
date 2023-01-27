5c82a9ef894c5e7cdb420122f238cd25579472cb
CanadaAbhi <57305692+CanadaAbhi@users.noreply.github.com>
2023-01-26 22:41:18 -0400

    Update I2C.c

--- I2C/I2C.c
+++ I2C/I2C.c
@@ -11,19 +11,19 @@
 #define DRIVER_NAME "bmp280"
 #define DRIVER_CLASS "bmp280Class"
 
-static struct i2c_adapter * bmp_i2c_adapter = NULL;
-static struct i2c_client * bmp280_i2c_client = NULL;
+static struct i2c_adapter * tempsensor_i2c_adapter = NULL;
+static struct i2c_client * tempsensor_i2c_client = NULL;
 
 /* Meta Information */
-MODULE_AUTHOR("Johannes 4Linux");
+MODULE_AUTHOR("AANM");
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("A driver for reading out a BMP280 temperature sensor");
+MODULE_DESCRIPTION("temperature sensor driver");
 MODULE_SUPPORTED_DEVICE("NONE");
 
 /* Defines for device identification */ 
-#define I2C_BUS_AVAILABLE	1		/* The I2C Bus available on the raspberry */
-#define SLAVE_DEVICE_NAME	"BMP280"	/* Device and Driver Name */
-#define BMP280_SLAVE_ADDRESS	0x76		/* BMP280 I2C address */
+#define I2C_BUS_AVAILABLE	1		                /* The I2C Bus available on the raspberry */
+#define SLAVE_DEVICE_NAME	"TEMP_SENSOR_BMP280"	        /* Device and Driver Name */
+#define TEMP_SENSOR_BMP280_SLAVE_ADDRESS	0x76		/* BMP280 I2C address */
 
 static const struct i2c_device_id bmp_id[] = {
 		{ SLAVE_DEVICE_NAME, 0 }, 
@@ -38,7 +38,7 @@ static struct i2c_driver bmp_driver = {
 };
 
 static struct i2c_board_info bmp_i2c_board_info = {
-	I2C_BOARD_INFO(SLAVE_DEVICE_NAME, BMP280_SLAVE_ADDRESS)
+	I2C_BOARD_INFO(SLAVE_DEVICE_NAME, TEMP_SENSOR_BMP280_SLAVE_ADDRESS)
 };
 
 /* Variables for Device and Deviceclass*/
@@ -47,36 +47,36 @@ static struct class *myClass;
 static struct cdev myDevice;
 
 /* Variables for temperature calculation */
-s32 dig_T1, dig_T2, dig_T3;
+s32 digital_T1, digital_T2, digital_T3;
 
 /**
  * @brief Read current temperature from BMP280 sensor
  *
  * @return temperature in degree
  */
-s32 read_temperature(void) {
-	int var1, var2;
-	s32 raw_temp;
-	s32 d1, d2, d3;
+s32 temperature_conversion(void) {
+	int r1, r2;
+	s32 temp_actual;
+	s32 smbus_d1, smbus_d2, smbus_d3;
 
 	/* Read Temperature */
-	d1 = i2c_smbus_read_byte_data(bmp280_i2c_client, 0xFA);
-	d2 = i2c_smbus_read_byte_data(bmp280_i2c_client, 0xFB);
-	d3 = i2c_smbus_read_byte_data(bmp280_i2c_client, 0xFC);
-	raw_temp = ((d1<<16) | (d2<<8) | d3) >> 4;
+	smbus_d1 = i2c_smbus_read_byte_data(bmp280_i2c_client, 0xFA);
+	smbus_d2 = i2c_smbus_read_byte_data(bmp280_i2c_client, 0xFB);
+	smbus_d3 = i2c_smbus_read_byte_data(bmp280_i2c_client, 0xFC);
+	temp_actual = ((smbus_d1<<16) | (smbus_d2<<8) | smbus_d3) >> 4;
 
 	/* Calculate temperature in degree */
-	var1 = ((((raw_temp >> 3) - (dig_T1 << 1))) * (dig_T2)) >> 11;
+	r1 = ((((temp_actual >> 3) - (digital_T1 << 1))) * (digital_T2)) >> 11;
 
-	var2 = (((((raw_temp >> 4) - (dig_T1)) * ((raw_temp >> 4) - (dig_T1))) >> 12) * (dig_T3)) >> 14;
-	return ((var1 + var2) *5 +128) >> 8;
+	r2 = (((((temp_actual >> 4) - (digital_T1)) * ((temp_actual >> 4) - (digital_T1))) >> 12) * (digital_T3)) >> 14;
+	return ((r1 + r2) *5 +128) >> 8;
 }
 
 /**
  * @brief Get data out of buffer
  */
 static ssize_t driver_read(struct file *File, char *user_buffer, size_t count, loff_t *offs) {
-	int to_copy, not_copied, delta;
+	int to_copy, byte_count, final_read_value;
 	char out_string[20];
 	int temperature;
 
@@ -84,23 +84,23 @@ static ssize_t driver_read(struct file *File, char *user_buffer, size_t count, l
 	to_copy = min(sizeof(out_string), count);
 
 	/* Get temperature */
-	temperature = read_temperature();
+	temperature = temperature_conversion();
 	snprintf(out_string, sizeof(out_string), "%d.%d\n", temperature/100, temperature%100);
 
 	/* Copy Data to user */
-	not_copied = copy_to_user(user_buffer, out_string, to_copy);
+	byte_count = copy_to_user(user_buffer, out_string, to_copy);
 
 	/* Calculate delta */
-	delta = to_copy - not_copied;
+	delta = to_copy - byte_count;
 
-	return delta;
+	return final_read_value;
 }
 
 /**
  * @brief This function is called, when the device file is opened
  */
 static int driver_open(struct inode *deviceFile, struct file *instance) {
-	printk("MyDeviceDriver -  Open was called\n");
+	printk("Custom driver Open called  \n");
 	return 0;
 }
 
@@ -108,7 +108,7 @@ static int driver_open(struct inode *deviceFile, struct file *instance) {
  * @brief This function is called, when the device file is close
  */
 static int driver_close(struct inode *deviceFile, struct file *instance) {
-	printk("MyDeviceDriver -  Close was called\n");
+	printk("Custom driver close called\n");
 	return 0;
 }
 
@@ -127,11 +127,11 @@ static struct file_operations fops = {
 static int __init ModuleInit(void) {
 	int ret = -1;
 	u8 id;
-	printk("MyDeviceDriver - Hello Kernel\n");
+	printk("Module Init called\n");
 
 	/* Allocate Device Nr */
 	if ( alloc_chrdev_region(&myDeviceNr, 0, 1, DRIVER_NAME) < 0) {
-		printk("Device Nr. could not be allocated!\n");
+		printk("Can't allocate device number!\n");
 	}
 	printk("MyDeviceDriver - Device Nr %d was registered\n", myDeviceNr);
 
@@ -152,43 +152,43 @@ static int __init ModuleInit(void) {
 
 	/* register device to kernel */
 	if (cdev_add(&myDevice, myDeviceNr, 1) == -1) {
-		printk("Registering of device to kernel failed!\n");
+		printk("Registering of device to linux kernel failed!\n");
 		goto KernelError;
 	}
 
-	bmp_i2c_adapter = i2c_get_adapter(I2C_BUS_AVAILABLE);
+	tempsensor_i2c_adapter = i2c_get_adapter(I2C_BUS_AVAILABLE);
 
-	if(bmp_i2c_adapter != NULL) {
-		bmp280_i2c_client = i2c_new_device(bmp_i2c_adapter, &bmp_i2c_board_info);
-		if(bmp280_i2c_client != NULL) {
+	if(tempsensor_i2c_adapter != NULL) {
+		tempsensor_i2c_client = i2c_new_device(tempsensor_i2c_adapter, &bmp_i2c_board_info);
+		if(tempsensor_i2c_client != NULL) {
 			if(i2c_add_driver(&bmp_driver) != -1) {
 				ret = 0;
 			}
 			else
 				printk("Can't add driver...\n");
 		}
-		i2c_put_adapter(bmp_i2c_adapter);
+		i2c_put_adapter(tempsensor_i2c_adapter);
 	}
-	printk("BMP280 Driver added!\n");
+	printk("Temp Sensor Driver added!\n");
 
 	/* Read Chip ID */
-	id = i2c_smbus_read_byte_data(bmp280_i2c_client, 0xD0);
+	id = i2c_smbus_read_byte_data(tempsensor_i2c_client, 0xD0);
 	printk("ID: 0x%x\n", id);
 
 	/* Read Calibration Values */
-	dig_T1 = i2c_smbus_read_word_data(bmp280_i2c_client, 0x88);
-	dig_T2 = i2c_smbus_read_word_data(bmp280_i2c_client, 0x8a);
-	dig_T3 = i2c_smbus_read_word_data(bmp280_i2c_client, 0x8c);
+	digital_T1 = i2c_smbus_read_word_data(tempsensor_i2c_client, 0x88);
+	digital_T2 = i2c_smbus_read_word_data(tempsensor_i2c_client, 0x8a);
+	digital_T3 = i2c_smbus_read_word_data(tempsensor_i2c_client, 0x8c);
 
-	if(dig_T2 > 32767)
-		dig_T2 -= 65536;
+	if(digital_T2 > 32767)
+		digital_T2 -= 65536;
 
-	if(dig_T3 > 32767)
-		dig_T3 -= 65536;
+	if(digital_T3 > 32767)
+		digital_T3 -= 65536;
 
 	/* Initialice the sensor */
-	i2c_smbus_write_byte_data(bmp280_i2c_client, 0xf5, 5<<5);
-	i2c_smbus_write_byte_data(bmp280_i2c_client, 0xf4, ((5<<5) | (5<<2) | (3<<0)));
+	i2c_smbus_write_byte_data(tempsensor_i2c_client, 0xf5, 5<<5);
+	i2c_smbus_write_byte_data(tempsensor_i2c_client, 0xf4, ((5<<5) | (5<<2) | (3<<0)));
 	return ret;
 
 KernelError:
@@ -206,7 +206,7 @@ ClassError:
  */
 static void __exit ModuleExit(void) {
 	printk("MyDeviceDriver - Goodbye, Kernel!\n");
-	i2c_unregister_device(bmp280_i2c_client);
+	i2c_unregister_device(tempsensor_i2c_client);
 	i2c_del_driver(&bmp_driver);
 	cdev_del(&myDevice);
     device_destroy(myClass, myDeviceNr);
